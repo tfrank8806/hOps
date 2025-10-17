8d9382204f3946b10c64d1b88078e0a20c53bd5c
Tyler Frankenberger <twfrankenberger@gmail.com>
2025-10-17 13:13:22 -0500

    Add Work Orders management page

---
+++ hOps.web/ViewModels/WorkOrders/WorkOrderStatusOption.cs
@@ -0,0 +1,9 @@
+namespace hOps.web.ViewModels.WorkOrders
+{
+    public class WorkOrderStatusOption
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Label { get; set; } = string.Empty;
+        public string ColorHex { get; set; } = "#6c757d";
+    }
+}
---
+++ hOps.web/ViewModels/WorkOrders/WorkOrdersViewModel.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using hOps.web.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace hOps.web.ViewModels.WorkOrders
+{
+    public class WorkOrdersViewModel
+    {
+        public List<WorkOrderListItemViewModel> WorkOrders { get; set; } = new();
+        public WorkOrderFilterInput Filters { get; set; } = new();
+        public WorkOrderFormViewModel Form { get; set; } = new();
+        public List<WorkOrderStatusOption> StatusOptions { get; set; } = new();
+        public List<Department> Departments { get; set; } = new();
+        public List<WorkOrderType> WorkOrderTypes { get; set; } = new();
+        public List<PropertyOptionViewModel> PropertyOptions { get; set; } = new();
+        public List<SelectListItem> CreatorOptions { get; set; } = new();
+        public List<string> LocationSuggestions { get; set; } = new();
+        public Dictionary<string, string> StatusColorMap { get; set; } = new();
+    }
+}
