56a1ac3b23264637da1690be17450983faec90be
Tyler Frankenberger <twfrankenberger@gmail.com>
2025-10-17 13:13:40 -0500

    Add pass on log management pages

---
+++ hOps.web/Models/PassOnLog.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace hOps.web.Models
+{
+    public class PassOnLog
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Body { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        [Required]
+        public string CreatedById { get; set; } = string.Empty;
+
+        public ApplicationUser? CreatedBy { get; set; }
+
+        public ICollection<PassOnLogProperty> Properties { get; set; } = new List<PassOnLogProperty>();
+
+        public ICollection<PassOnLogComment> Comments { get; set; } = new List<PassOnLogComment>();
+
+        public ICollection<PassOnLogView> Views { get; set; } = new List<PassOnLogView>();
+    }
+}
