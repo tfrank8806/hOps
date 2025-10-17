69ac3633874bb79ecc0b7fead8550cf05631f435
tfrank8806 <twfrankenberger@gmail.com>
2025-10-17 12:17:21 -0500

    build error fix

--- hOps.web.Tests/obj/Release/net8.0/hOps.web.Tests.AssemblyInfo.cs
+++ hOps.web.Tests/obj/Release/net8.0/hOps.web.Tests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("hOps.web.Tests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe8fc0e99d579d3695ffa862e8e606734411ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("hOps.web.Tests")]
