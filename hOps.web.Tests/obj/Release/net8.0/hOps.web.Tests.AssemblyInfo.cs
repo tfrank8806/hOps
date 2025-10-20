087eab06cd5572d8f7fc0a2600e65364d7e8a31c
tfrank8806 <twfrankenberger@gmail.com>
2025-10-20 12:54:31 -0500

    hOps Title

--- hOps.web.Tests/obj/Release/net8.0/hOps.web.Tests.AssemblyInfo.cs
+++ hOps.web.Tests/obj/Release/net8.0/hOps.web.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe8fc0e99d579d3695ffa862e8e606734411ed1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f96a6ac61129a84cf1e7a827f2db0856a6e564")]
 [assembly: System.Reflection.AssemblyProductAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
