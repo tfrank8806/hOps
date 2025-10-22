3429e8abcdbfb84fcef1543231246c95be48721f
Tyler Frankenberger <twfrankenberger@gmail.com>
2025-10-21 20:22:10 -0500

    Fix Bookmark Section property type in migration snapshot

--- hOps.web/Migrations/20251026000000_AddHomePageContent.Designer.cs
+++ hOps.web/Migrations/20251026000000_AddHomePageContent.Designer.cs
@@ -417,7 +417,7 @@ namespace hOps.web.Migrations
                     b.Property<int?>("PropertyId")
                         .HasColumnType("INTEGER");
 
-                    b.Property<int>("Section")
+                    b.Property<BookmarkSection>("Section")
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("Url")
