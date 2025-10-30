50c4a6e584930eb70e01f7854d6b3305af3c469a
tfrank8806 <twfrankenberger@gmail.com>
2025-10-30 08:53:33 -0500

    move room layout save button to top of page

--- hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
+++ hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724f18d696ecc099fdcf6fd84c1d5d4d75b69ac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2fde313510eef9444fd07e59b5b6f8bbaa85cd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
