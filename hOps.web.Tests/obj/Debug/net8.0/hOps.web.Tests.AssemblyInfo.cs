faf7c10cd519ace28fc72092945cb584c0c3a629
tfrank8806 <twfrankenberger@gmail.com>
2025-10-30 21:02:53 -0500

    sidebar collapse dotnet build

--- hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
+++ hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8ba7b883a6695c76ed58193b0640b4698f9f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fdc503d22aef9666c643351ee0aca768e1f1f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
