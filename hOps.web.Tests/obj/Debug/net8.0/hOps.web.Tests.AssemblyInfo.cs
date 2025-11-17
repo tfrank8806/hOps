d349e5cef8e0cf0e76cba6fb865d2f80f890c738
tfrank8806 <twfrankenberger@gmail.com>
2025-11-17 15:11:36 -0600

    commit

--- hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
+++ hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd5484a7922634713e599daeccbed8d3f45014a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625aefa3479544b762018d1fb4ee573f9e17fed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
