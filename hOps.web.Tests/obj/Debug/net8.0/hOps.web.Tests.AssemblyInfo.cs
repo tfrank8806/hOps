5acf4fe63b8f2079c28e7bf9bd8b4981482b2601
tfrank8806 <twfrankenberger@gmail.com>
2025-11-18 07:56:32 -0600

    • I enabled stdout logging locally by adding hOps.web/web.config (stdoutLogEnabled="true").

--- hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
+++ hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c7648145e62852d53fbd78f703a98a0fd4163f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db88bbe128d37168845deb0023f46c8ef753a122")]
 [assembly: System.Reflection.AssemblyProductAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
