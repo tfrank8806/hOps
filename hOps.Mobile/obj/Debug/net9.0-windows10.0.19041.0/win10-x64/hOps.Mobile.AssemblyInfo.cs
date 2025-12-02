a7949c9e55231e34365a411cba7cead4feaad4be
tfrank8806 <twfrankenberger@gmail.com>
2025-12-02 16:38:29 -0600

    migration

--- hOps.Mobile/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/hOps.Mobile.AssemblyInfo.cs
+++ hOps.Mobile/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/hOps.Mobile.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hOps.Mobile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9a8ea621d5b5910fc8c2530dbd57bc19812fa59d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+14b493c0252340751a714d2ec707e461d3e89730")]
 [assembly: System.Reflection.AssemblyProductAttribute("hOps.Mobile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hOps.Mobile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
