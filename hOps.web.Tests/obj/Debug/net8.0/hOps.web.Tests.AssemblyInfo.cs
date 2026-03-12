4936a469a61b519eb080c2c4a40ca850a0cf4c6c
tfrank8806 <twfrankenberger@gmail.com>
2026-03-12 06:28:53 -0500

    PROPERTY_NAME MERCHANT_ID TRANSACTION_DATE PROCESSOR CARD_BRAND MASKED_CARD_NO AUTHORIZATION_CODE Sum of FORCED_AMOUNT Aloft Houston Shenandoah 8042804404 09/03/2026 ELAVON VISA ************1608 298 138.77 Aloft Houston Shenandoah Total       138.77 Grand Total       138.77

--- hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
+++ hOps.web.Tests/obj/Debug/net8.0/hOps.web.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9dd3656e2317318e0f0003b2be3d1a0efa22b9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a302ceee98cd9aeee952babf0f81e82f617b00d")]
 [assembly: System.Reflection.AssemblyProductAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hOps.web.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
